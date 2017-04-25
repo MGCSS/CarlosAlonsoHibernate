250b0b21e4d84f2f2e32f8bfc19ae8b8761a93b5
Carlos <Carlos@Carlos-PC>
2017-04-25 18:56:30 +0200

    Parte 2 hecha

---
+++ src/practicahibernate/PracticaHibernate.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practicahibernate;
+
+import modelo.Experto;
+
+/**
+ *
+ * @author Carlos
+ */
+public class PracticaHibernate {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      ManejaExperto me = new ManejaExperto();     
+     
+      
+    Experto ex = me.obtenExperto("E001");     
+    System.out.println(ex.getEspecialidad());
+    System.out.println(ex.getNombre());
+     
+     //me.obtenCasos();
+    }
+    
+}
